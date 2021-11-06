5638ebc956b36f146c611ce4538917c691331c9f
hamidrehzaey <hamidrehzaey@gmail.com>
2021-11-07 02:12:08 +0330

    تغییر در فایل views.py

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-from .import models
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import article
 
 def home(request):
     return render(request, 'articles/home.html')
@@ -10,9 +9,13 @@ def about(request):
 
 def ArticlesList(request):
     global articles
-    articles = models.article.objects.all().order_by('create_date')
+    articles = article.objects.all().order_by('create_date')
     return render(request, 'articles/ArticlesList.html', {'articles':articles})
 
 def ArticleContinu(request, arc_id):
-    articles1 = models.article.objects.get(pk=arc_id)
+    try:
+        articles1 = get_object_or_404(article, pk=arc_id)
+    except:
+        return HttpResponse('صفحه مورد نظر پیدا نشد.')
+
     return render(request, 'articles/ArticleContinu.html', {'articles':articles1})
